206b349534f9af5d0da58491c269f0350b7e21f9
James Nguyen <James@hourglassets.com>
2020-09-15 16:22:50 -0700

    Updates

---
+++ precache-manifest.2e7211f9b3d5db280d0c3207e376f41a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab444954b8bfb27cc666df1e80c6fb4",
+    "url": "/one/index.html"
+  },
+  {
+    "revision": "90b60cf0d40c42e181a0",
+    "url": "/one/static/css/main.e9052217.chunk.css"
+  },
+  {
+    "revision": "07dc8b001a7d66194d9d",
+    "url": "/one/static/js/2.d945f6bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/one/static/js/2.d945f6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b60cf0d40c42e181a0",
+    "url": "/one/static/js/main.610170b8.chunk.js"
+  },
+  {
+    "revision": "1401ac461431fb05ae74",
+    "url": "/one/static/js/runtime-main.da641153.js"
+  },
+  {
+    "revision": "af26789defebc5ddbe0e4df754a00a83",
+    "url": "/one/static/media/logo.af26789d.svg"
+  }
+]);
